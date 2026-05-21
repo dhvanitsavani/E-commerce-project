116cc4013c0ab80d5d27c67718fbaa67f155d9a7
dhvanitsavani <dhvanit4950@gmail.com>
2026-05-21 23:59:27 +0530

    e-commerce

--- myapp/models.py
+++ myapp/models.py
@@ -9,3 +9,12 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Seller(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    mobile = models.PositiveIntegerField()
+    password = models.CharField(max_length=50)
+    company_name = models.CharField(max_length=50)
+    aadhar_card_no = models.PositiveBigIntegerField()
+    address = models.TextField()
